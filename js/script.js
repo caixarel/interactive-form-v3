4109dced7f663c7303a435338aa37f92d60b508b
caixarel <44868812+caixarel@users.noreply.github.com>
2021-06-19 12:48:18 +0200

    4

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let creditCardDiv = document.getElementById("credit-card")
 let paypalDiv = document.getElementById("paypal");
 let bitcoinDiv = document.getElementById("bitcoin");
 let activitiesStyle =document.querySelectorAll("input[type='checkbox']");
-
+let selectedActivities= [];
 //the 'color' options will be disabled until the user selects the shirts design
 colorEle.disabled = true;
 nameInput.focus();
@@ -30,7 +30,6 @@ bitcoinDiv.style.display = "none";
 payment.children[1].setAttribute("selected",true);
 //loop to give focus or not to the activities
 for(let i =0;i<activitiesStyle.length;i++){
-    console.log(activitiesStyle[i].checked);
     activitiesStyle[i].addEventListener('focus',(e)=>{
         activitiesStyle[i].parentNode.classList.add('focus');
     })
@@ -38,57 +37,129 @@ for(let i =0;i<activitiesStyle.length;i++){
         activitiesStyle[i].parentNode.classList.remove('focus');
     })
 }
+nameInput.addEventListener("input",(e)=>{
+    nameValidity();
+})
+cardNumber.addEventListener("input",(e)=>{
+    cardValidity();
+})
+zipCode.addEventListener("input",(e)=>{
+    zipValidity();
+})
+cvvNumber.addEventListener("input",(e)=>{
+    cvvValidity();
+})
+function nameValidity(){
+    let nameValue = nameInput.value;
+    let nameRegEx = /^[a-zspace]+$/.test(nameValue);
+    if(!nameRegEx){
+        invalidInput(nameInput);
+        let errorMessage =nameInput.parentNode.lastElementChild;
+        errorMessage.innerHTML = "Name must contain only letters";
+    }
+    else{
+        validInput(nameInput);
+    }
+    return nameRegEx;
+}
+function mailValidity(){
+    let emailValue =emailAddress.value;
+    return /^[^@]+@[^@.]+\.[a-zA-Z]+$/.test(emailValue);
+}
+function cardValidity(){
+    let cardNumberValue =cardNumber.value;
+    cardRegEx =/^[0-9]{14,16}$/.test(cardNumberValue);
+    if(/[a-zA-Z]/.test(cardNumberValue)){
+        invalidInput(cardNumber);
+        let errorMessage =cardNumber.parentNode.lastElementChild;
+        errorMessage.innerHTML = "Card number must contain only numbers";
+    }
+    else if(cardRegEx){
+        validInput(cardNumber);
+    }
+    else{
+        invalidInput(cardNumber);
+        let errorMessage =cardNumber.parentNode.lastElementChild;
+        errorMessage.innerHTML = "Credit card number must be between 13 - 16 digits";
+    }
+    return cardRegEx;
+}
+function zipValidity(){
+    let zipCodeValue = zipCode.value;
+    zipRegEx =/^[0-9]{5}$/.test(zipCodeValue);
+    if(/[a-zA-Z]/.test(zipCodeValue)){
+        invalidInput(zipCode);
+        let errorMessage =zipCode.parentNode.lastElementChild;
+        errorMessage.innerHTML = "Zip code must contain only numbers";
+    }
+    else if(zipRegEx){
+        validInput(zipCode);
+    }
+    else{
+        invalidInput(zipCode);
+        let errorMessage =zipCode.parentNode.lastElementChild;
+        errorMessage.innerHTML = "Zip Code must be 5 digits";
+    }
+    return zipRegEx;
+}
+function cvvValidity(){
+    let cvvValue = cvvNumber.value;
+    let cvvRegEx= /^[0-9]{3}$/.test(cvvValue);
+    if(/[a-zA-Z]/.test(cvvValue)){
+        invalidInput(cvvNumber);
+        let errorMessage =cvvNumber.parentNode.lastElementChild;
+        errorMessage.innerHTML = "CVV must contain only numbers";
+    }
+    else if(cvvRegEx){
+        validInput(cvvNumber);
+    }
+    else{
+        invalidInput(cvvNumber);
+        let errorMessage =cvvNumber.parentNode.lastElementChild;
+        errorMessage.innerHTML = "CVV must be 3 digits";
+    }
+    return cvvRegEx;
+}
+
+function activitiesValidity(bol){
+    console.log(bol);
+}
+
 //event that will verify if the form can be validated or not
 formElement.addEventListener("submit", (e)=>{
     //verifyng if the input fields are valid using regular expressions
-    let nameValue = nameInput.value; 
-    let validName = /^[space a-z]+$/.test(nameValue);
-    let emailValue =emailAddress.value;
-    let validEmail = /^[^@]+@[^@.]+\.[a-zA-Z]+$/.test(emailValue);
-    let cardNumberValue =cardNumber.value;
-    let validCarNumber =/^[0-9]{14,16}$/.test(cardNumberValue);
-    let zipCodeValue = zipCode.value;
-    let validZipCode = /^[0-9]{5}$/.test(zipCodeValue)
-    let cvvValue = cvvNumber.value;
-    let validCvv = /^[0-9]{3}$/.test(cvvValue);
+    let validName = nameValidity();  
+    let validEmail = mailValidity();
+    let validCarNumber = cardValidity();
+    let validZipCode = zipValidity();
+    let validCvv = cvvValidity();
+    let validActivities = activitiesValidity();
     if (payment.value == "credit-card"){
         if(validName && validEmail && totalCost!=0 && validCarNumber && validZipCode &&validCvv ){
         }
         else{
             e.preventDefault();
             //if not possible to validate the form then every not valid field will display a error message 
-            if (!validName){
-                validInput(nameInput);
-            }
-            else{
-                invalidInput(nameInput);
-            }
             if(!validEmail){
-                validInput(emailAddress);
-            }
-            else{
                 invalidInput(emailAddress);
             }
+            else{
+                validInput(emailAddress);
+            }
             if(totalCost==0){
+                invalidInput(activitiesField.children[0]);
+            }
+            else{
                 validInput(activitiesField.children[0]);
             }
-            else{
-                validInput(activitiesField);
-            }
-            if(!validCarNumber){
-                validInput(cardNumber);
-            }
-            else{
-                validInput(cardNumber);
-            }
             if(!validZipCode){
-                validInput(zipCode);
+                invalidInput(zipCode);
             }
             else{
                 validInput(zipCode);
             }
             if(!validCvv){
-                validInput(cvvNumber);
+                invalidInput(cvvNumber);
             }
             else{
                 validInput(cvvNumber);
@@ -100,20 +171,14 @@ formElement.addEventListener("submit", (e)=>{
             
         }
         else{
-            if (!validName){
-                validInput(nameInput);
-            }
-            else{
-                invalidInput(nameInput);
-            }
             if(!validEmail){
-                validInput(emailAddress);
-            }
-            else{
                 invalidInput(emailAddress);
             }
+            else{
+                validInput(emailAddress);
+            }
             if(totalCost==0){
-                validInput(activitiesField.children[0]);
+                invalidInput(activitiesField.children[0]);
             }
             else{
                 validInput(activitiesField);
@@ -123,12 +188,12 @@ formElement.addEventListener("submit", (e)=>{
     }
 })
 //functions to display valid or invalid messages to the user
-function validInput(input){
+function invalidInput(input){
         input.parentNode.classList.add("not-valid");
         input.parentNode.classList.remove("valid");
         input.parentNode.lastElementChild.style.display ="block"; 
 }
-function invalidInput(input){
+function validInput(input){
     input.parentNode.classList.add("valid");
     input.parentNode.classList.remove("not-valid");
     input.parentNode.lastElementChild.style.display ="none"; 
@@ -157,11 +222,31 @@ payment.addEventListener("change", (e)=>{
 //update the total price when activities are choosen 
 activitiesField.addEventListener("change", (e)=>{
     let cost = +e.target.getAttribute("data-cost");
+    let activityName = e.target.getAttribute("name")
+    let activityDate = e.target.getAttribute("data-day-and-time").toString();
+    let activityError = false;
+    let matchCount = 0;
     if (e.target.checked){
-        totalCost += cost;
+        totalCost += cost; 
+        selectedActivities.push(e.target);
+        for(let i = 0;i<selectedActivities.length;i++){
+            if(activityDate===selectedActivities[i].getAttribute("data-day-and-time") && activityName!==selectedActivities[i].getAttribute("name")){
+                console.log("diferent activity");
+            }
+        }
+        activitiesValidity(activityError);
     }
     else{
         totalCost-=cost;
+        for(let i = 0 ;i<selectedActivities.length;i++){
+            if(activityName===selectedActivities[i].getAttribute("name")){
+            selectedActivities.splice(i,1);
+            }
+            if(selectedActivities.length>0 && !selectedActivities.includes(activityDate)){
+                activitiesValidity(true);
+        }
+        
+        }
     }
     totalCostParagraph.innerHTML = `Total ${totalCost}€`;
 })
